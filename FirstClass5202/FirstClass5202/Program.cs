701f327abb45562094d1e679b96b8bde7024f35e
Virginia Barnett <30027159@student.toiohomai.ac.nz>
2019-07-24 11:28:51 +1200

    Hello World

---
+++ FirstClass5202/FirstClass5202/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass5202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please Enter Your Last Name: ");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}, nice to meet you.", name, lname);
+            Console.WriteLine("Hello " + name + " " + lname + ", nice to meet you. ");
+            Console.WriteLine($"Hello {name} {lname}, nice to meet you. ");
+            Console.ReadLine();
+            Console.WriteLine("How are you today: ");
+        }
+    }
+}
